7f595ba716ebaf082d313536d05c48003f527f21
digisdeus <jhester@paramountfarms.com>
2014-02-10 08:17:38 -0800

    Upgrade
    Fixed a bug that happened when there was more than one <ul> element on
    the page that used pfiTree.
    Also cleaned up and compacted some code blocks

--- pfiTree.js
+++ pfiTree.js
@@ -1,34 +1,41 @@
-/*! ****************************************************************************
-	pfiTree
-	tree view control, with support for nested lists
-	Dual licensed under the MIT or GPL Version 2 licenses.
-
-	@version: 1.0.0
-	@date:    2014-07-02
-	@depends: jquery.js
-*******************************************************************************/
-var pfiTree = {
-	makeTree:function(ele){
-		ele.children().each(function(){
-			if($(this).children().length >= 1){
-				$(this).addClass('parent-closed');
-				return true;
-			}else{
-				$(this).addClass('child');
+function makeTree(ele){
+	ele.children().each(function(){
+		if($(this).children().length >= 1){
+			$(this).addClass('parent-closed');
+			makeTree($(this));
+			return true;
+		}else{
+			$(this).addClass('child');
+		}
+	});
+	//this code will 'collapse' the parent node
+	$.each(ele.children(),function(){
+		var l = $(this).find('ul');
+		l.toggle();
+	});
+	//this code will assign click events to parent elements
+	$.each(ele.children(), function(){
+		if(ele.hasClass('parent-closed') || ele.hasClass('parent-open')){
+			$(this).unbind('click').bind('click', function(e){
+				e.preventDefault();
+				if($(this).hasClass('parent-closed')){$(this).removeClass('parent-closed').addClass('parent-open')}else{$(this).removeClass('parent-open').addClass('parent-closed')}
+				$(this).find('ul').toggle();
+				e.stopPropagation();
+			});
+		}
+	});
+	//This assigns the navigation to the child elements if the li clicked has a data-app attribute
+	$('.child').unbind('click').bind('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		if($(this).data('app')){
+			var name = window.navigator.appName;
+			if(name == 'Microsoft Internet Explorer'){
+				window.navigate($(this).data('app'));
 			}
-		});
-		$('.parent-closed > ul').toggle();
-		$('.parent-closed, .parent-open').bind('click', function(e){
-			var obj = $('.parent-closed, .parent-open').eq($('.parent-closed, .parent-open').index($(this)));
-			e.preventDefault();
-			if(obj.hasClass('parent-closed'))
-			{
-				obj.removeClass('parent-closed').addClass('parent-open');
-			}else{
-				obj.removeClass('parent-open').addClass('parent-closed');
+			else{
+				window.open($(this).data('app'));
 			}
-			obj.children().toggle();
-			e.stopPropagation();
-		});
-	}
+		}
+	});
 }
